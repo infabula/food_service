1fec4fd0cc7e56de821afc32898b854a0e5319f7
jeroen@infabula.nl <bitbucket@infabula.nl>
2018-03-07 10:34:18 +0100

    minimal food_service package

---
+++ food_service/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='food_service',
+    packages=['food_service'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
